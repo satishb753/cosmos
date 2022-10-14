483d50c52b122b8c39e00362c3094ec1b32b7e61
satishb753 <satishb753@gmail.com>
2022-10-15 04:35:15 +0530

    Updated Express app to accept https requests(Self-signed SSL Certificate)

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+const fs = require('fs')
+const https = require('https')
 const express = require('express');
-const cluster = require('cluster');
-const os = require('os');
+const path = require('path')
+
+const certificate = fs.readFileSync('./cert.pem');
+const privateKey = fs.readFileSync('./key.pem');
 
 const app = express();
+const PORT = 8443;
 
 function delay(duration) {
   const startTime = Date.now();
@@ -12,25 +17,17 @@ function delay(duration) {
 }
 
 app.get('/', (req, res) => {
-  // JSON.stringify({}) => "{}"
-  // JSON.parse("{}") => {}
-  // [5,1,2,3,4].sort()
   res.send(`Performance example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
-  delay(9000);
-  res.send(`Ding ding ding! ${process.pid}`);
+  delay(4000);
+  res.send(`Beep beep beep! ${process.pid}`);
 });
 
-console.log('Running server.js...');
-if (cluster.isMaster) {
-  console.log('Master has been started...');
-  const NUM_WORKERS = os.cpus().length;
-  for (let i = 0; i < NUM_WORKERS; i++) {
-    cluster.fork();
-  }
-} else {
-  console.log('Worker process started.');
-  app.listen(3000);
-}
+https.createServer({
+  key: privateKey,
+  cert: certificate
+}, app).listen(PORT, ()=>{
+  console.log(`Listening on port ${PORT}`)
+})
