f982439729f8803dfb5ddaa83edb9e855c8143d7
satishb753 <satishb753@gmail.com>
2022-10-15 14:26:20 +0530

    Passport, Passport-google-oauth20, dotenv installed

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 const fs = require('fs')
 const https = require('https')
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const helmet = require('helmet');
 
 const certificate = fs.readFileSync('./cert.pem');
 const privateKey = fs.readFileSync('./key.pem');
+const PORT = 8443;
 
 const app = express();
-const PORT = 8443;
+app.use(helmet());
+
+const config = {
+  CLIENT_ID: '468398194208-f9bebfbmp57e1e6g5g4232aschivhsa5.apps.googleusercontent.com',
+  CLIENT_SECRET: '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',
+}
+
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true;  //TODO
+  if (!isLoggedIn)  {
+    return res.status(401).json({
+      error: 'You must log in!',
+    })
+  }
+  next();
+}
+
+app.get('/auth/google', (req, res) => {});
+
+app.get('/auth/google/callback', (req, res) => {});
+
+app.get('/auth/logout', (req, res) => {});
+
+app.get('/secret', checkLoggedIn, (req, res) => {
+  return res.status(200).json({
+    message: 'You are past the Oauth client! Congrats!',
+  })
+})
 
 function delay(duration) {
   const startTime = Date.now();
