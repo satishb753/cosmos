521f24cc95395a6397a4f6aa4583b3a617f8135c
satishb753 <satishb753@gmail.com>
2022-10-15 21:00:47 +0530

    Google Login implemented

--- server.js
+++ server.js
@@ -3,18 +3,34 @@ const https = require('https')
 const express = require('express');
 const path = require('path');
 const helmet = require('helmet');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
+const { verify } = require('crypto');
+const { fileURLToPath } = require('url');
+
+require('dotenv').config();
 
 const certificate = fs.readFileSync('./cert.pem');
 const privateKey = fs.readFileSync('./key.pem');
 const PORT = 8443;
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+}
+
+const AUTH_OPTIONS = {
+  callbackURL:"/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
 
 const app = express();
-app.use(helmet());
 
-const config = {
-  CLIENT_ID: '468398194208-f9bebfbmp57e1e6g5g4232aschivhsa5.apps.googleusercontent.com',
-  CLIENT_SECRET: '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',
-}
+app.use(helmet());
+app.use(passport.initialize());
+
 
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true;  //TODO
@@ -26,9 +42,27 @@ function checkLoggedIn(req, res, next) {
   next();
 }
 
-app.get('/auth/google', (req, res) => {});
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log('Google Profile', profile);
+  done(null, profile);
+}
 
-app.get('/auth/google/callback', (req, res) => {});
+app.get('/auth/google', 
+  passport.authenticate('google', {
+    scope: ['email'],
+  })
+);
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session:false,
+  }), 
+  (req, res) => {
+    console.log("Google called back!")
+  }
+);
 
 app.get('/auth/logout', (req, res) => {});
 
@@ -36,6 +70,10 @@ app.get('/secret', checkLoggedIn, (req, res) => {
   return res.status(200).json({
     message: 'You are past the Oauth client! Congrats!',
   })
+})
+
+app.get('/failure', (req, res) => {
+  return res.send('Failed to log in!');
 })
 
 function delay(duration) {
@@ -46,7 +84,14 @@ function delay(duration) {
 }
 
 app.get('/', (req, res) => {
-  res.send(`Performance example: ${process.pid}`);
+  const options = {
+    root: path.join(__dirname)
+  }
+  // res.send(`Performance example: ${process.pid}`);
+  res.sendFile('/public/index.html',options, (err) => {
+    if(err) next(err)
+    else console.log("Sent: index.html")
+  });
 });
 
 app.get('/timer', (req, res) => {
