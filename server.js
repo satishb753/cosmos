c5efa172ce8b8cda0593556b0fc5f4a511b57746
satishb753 <satishb753@gmail.com>
2022-11-10 21:03:59 +0530

    updated some files.

--- server.js
+++ server.js
@@ -1,39 +1,39 @@
-const fs = require('fs')
-const https = require('https')
-const express = require('express');
-const path = require('path');
-const helmet = require('helmet');
-const passport = require('passport');
-const { Strategy } = require('passport-google-oauth20');
-const { verify } = require('crypto');
-const { fileURLToPath } = require('url');
-const cookieSession = require('cookie-session');
+const fs = require("fs");
+const https = require("https");
+const express = require("express");
+const path = require("path");
+const helmet = require("helmet");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
+const { verify } = require("crypto");
+const { fileURLToPath } = require("url");
+const cookieSession = require("cookie-session");
+const { check, body } = require("express-validator");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const certificate = fs.readFileSync('./cert.pem');
-const privateKey = fs.readFileSync('./key.pem');
+const certificate = fs.readFileSync("./cert.pem");
+const privateKey = fs.readFileSync("./key.pem");
 const PORT = 8443;
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
   COOKIE_KEY_1: process.env.COOKIE_KEY_1,
   COOKIE_KEY_2: process.env.COOKIE_KEY_2,
-}
+};
 
 const AUTH_OPTIONS = {
-  callbackURL:"/auth/google/callback",
+  callbackURL: "/auth/google/callback",
   clientID: config.CLIENT_ID,
   clientSecret: config.CLIENT_SECRET,
 };
 
-passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
-
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
 // Save the session to the cookie
 passport.serializeUser((user, done) => {
   done(null, user.id);
-})
+});
 
 // Load the session from the cookie
 passport.deserializeUser((id, done) => {
@@ -41,84 +41,115 @@ passport.deserializeUser((id, done) => {
   //   done((null, user));
   // })
   done(null, id);
-})
+});
 
 const app = express();
 
 app.use(helmet());
 
-app.use(cookieSession({
-  name:'session',
-  maxAge: 24*60*60*1000,
-  keys: [ config.COOKIE_KEY_1, config.COOKIE_KEY_2 ]
-}))
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 function checkLoggedIn(req, res, next) {
   console.log("Current user is: ", req.user);
-  const isLoggedIn = req.isAuthenticated() && req.user; 
-  if (!isLoggedIn)  {
+  const isLoggedIn = req.isAuthenticated() && req.user;
+  if (!isLoggedIn) {
     return res.status(401).json({
-      error: 'You must log in!',
-    })
+      error: "You must log in!",
+    });
   }
   next();
 }
 
 function verifyCallback(accessToken, refreshToken, profile, done) {
-  console.log('Google Profile', profile);
+  console.log("Google Profile", profile);
   done(null, profile);
 }
 
-app.get('/auth/google', 
-  passport.authenticate('google', {
-    scope: ['email'],
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"],
   })
 );
 
-app.get('/auth/google/callback', 
-  passport.authenticate('google', {
-    failureRedirect: '/failure',
-    successRedirect: '/',
-    session:true,
-  }), 
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: true,
+  }),
   (req, res) => {
-    console.log("Google called back!")
+    console.log("Google called back!");
   }
 );
 
-app.get('/auth/logout', (req, res) => {
+app.get("/auth/logout", (req, res) => {
   req.logout();
-  return res.redirect('/');
+  return res.redirect("/");
 });
 
-app.get('/secret', checkLoggedIn, (req, res) => {
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.status(200).json({
-    message: 'You are past the Oauth client! Congrats!',
-  })
-})
-
-app.get('/failure', (req, res) => {
-  return res.send('Failed to log in!');
-})
-
-
-app.get('/', (req, res) => {
-  const options = {
-    root: path.join(__dirname)
-  }
-  // res.send(`Performance example: ${process.pid}`);
-  res.sendFile('/public/index.html',options, (err) => {
-    if(err) next(err)
-    else console.log("Sent: index.html")
+    message: "You are past the Oauth client! Congrats!",
   });
 });
 
-https.createServer({
-  key: privateKey,
-  cert: certificate
-}, app).listen(PORT, ()=>{
-  console.log(`Listening on port ${PORT}`)
-})
+app.get("/failure", (req, res) => {
+  return res.send("Failed to log in!");
+});
+
+app.get("/", (req, res) => {
+  const options = {
+    root: path.join(__dirname),
+  };
+  // res.send(`Performance example: ${process.pid}`);
+  res.sendFile("/public/index.html", options, (err) => {
+    if (err) next(err);
+    else console.log("Sent: index.html");
+  });
+});
+
+app.post(
+  "/validate",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter valid string")
+      .custom((value, { req }) => {
+        if (value === "test@test.com") {
+          console.log(req.body);
+          throw new Error("This value if forbidden");
+        }
+        // console.log(req);
+        return true;
+      }),
+    body("password").isLength({min:5}).isAlphanumeric()
+  ],
+  (req, res) => {
+    console.log(req.body.password);
+    console.log("It is a valid email");
+    res.status(200).json({ message: "You have recieved the data." });
+  }
+);
+
+https
+  .createServer(
+    {
+      key: privateKey,
+      cert: certificate,
+    },
+    app
+  )
+  .listen(PORT, async () => {
+    console.log(`Listening on port ${PORT}`);
+  });
